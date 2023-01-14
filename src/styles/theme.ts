167387cd836806a528d2ed9c114bfc204ad49a79
Enderson Barreto <enderson.frazao7@gmail.com>
2023-01-14 17:41:45 -0300

    feat: theme lightslategrey alterado

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,9 @@
 const theme = {
   background: '#11172B',
   backgroundLight: '#1B2138',
+  backgroundWhite: '#fff',
   primary: '#0EE7B7',
-  secondary: '#7AC7E3',
+  secondary: '#82DEE3',
   gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
   text: '#848DA0',
   textHighlight: '#444B5B',
@@ -10,7 +11,7 @@ const theme = {
   border: '#313958',
   inputBackground: '#1E253E',
   error: '#d45d5d',
-  lightslategrey: '#64748B',
+  lightslategrey: '#4C5767',
   background3: 'linear-gradient(to right, #41295a, #2f0743)'
 };
 
