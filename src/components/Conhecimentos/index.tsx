25d78fb5ec9e4c47f509ea03b9ebfd701fbbe650
Enderson Barreto <enderson.frazao7@gmail.com>
2023-01-19 14:08:01 -0300

    feat: removendo alguns logo

--- src/components/Conhecimentos/index.tsx
+++ src/components/Conhecimentos/index.tsx
@@ -1,15 +1,18 @@
 import { AiFillHtml5 } from 'react-icons/ai';
 import { DiGit } from 'react-icons/di';
-import { FaCss3Alt, FaReact, FaWindows, FaLinux, FaNpm } from 'react-icons/fa';
-import { IoLogoJavascript, IoLogoNodejs, IoLogoPython } from 'react-icons/io5';
+import { FaCss3Alt, FaReact } from 'react-icons/fa';
 import {
-  SiPostgresql,
+  IoLogoJavascript,
+  IoLogoPython,
+  IoLogoNodejs,
+  IoLogoSass
+} from 'react-icons/io5';
+import {
   SiTypescript,
   SiNextdotjs,
   SiBlockchaindotcom,
-  SiVisualstudiocode
+  SiStyledcomponents
 } from 'react-icons/si';
-import { GrMysql } from 'react-icons/gr';
 import SectionTitle from '../SectionTitle';
 import ConhecimentoItem from './ConhecimentoItem';
 import { Container } from './styles';
@@ -19,22 +22,21 @@ function Conhecimentos() {
     <Container>
       <SectionTitle title="Conhecimentos" />
       <section>
-        <ConhecimentoItem title="Javascript" icon={<IoLogoJavascript />} />
         <ConhecimentoItem title="HTML" icon={<AiFillHtml5 />} />
         <ConhecimentoItem title="CSS" icon={<FaCss3Alt />} />
-        <ConhecimentoItem title="PostgreSQL" icon={<SiPostgresql />} />
+        <ConhecimentoItem title="Javascript" icon={<IoLogoJavascript />} />
         <ConhecimentoItem title="TypeScript" icon={<SiTypescript />} />
-        <ConhecimentoItem title="MySQL" icon={<GrMysql />} />
+        <ConhecimentoItem title="NodeJS" icon={<IoLogoNodejs />} />
         <ConhecimentoItem title="React" icon={<FaReact />} />
         <ConhecimentoItem title="NextJS" icon={<SiNextdotjs />} />
         <ConhecimentoItem title="Blockchain" icon={<SiBlockchaindotcom />} />
         <ConhecimentoItem title="Git" icon={<DiGit />} />
-        <ConhecimentoItem title="NodeJS" icon={<IoLogoNodejs />} />
+        <ConhecimentoItem title="Sass" icon={<IoLogoSass />} />
+        <ConhecimentoItem
+          title="Styled Components"
+          icon={<SiStyledcomponents />}
+        />
         <ConhecimentoItem title="Python" icon={<IoLogoPython />} />
-        <ConhecimentoItem title="VS Code" icon={<SiVisualstudiocode />} />
-        <ConhecimentoItem title="Npm" icon={<FaNpm />} />
-        <ConhecimentoItem title="Windows" icon={<FaWindows />} />
-        <ConhecimentoItem title="Linux" icon={<FaLinux />} />
       </section>
     </Container>
   );
