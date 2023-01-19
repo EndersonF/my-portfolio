0c972a92662ece5d9a2ac88dbfcb3d23dc83161e
Enderson Barreto <enderson.frazao7@gmail.com>
2023-01-19 14:06:23 -0300

    feat: function sobre

---
+++ src/components/Sobre/SobreItem.tsx
@@ -0,0 +1,35 @@
+import { Container } from './styles';
+
+export default function SobreItem() {
+  return (
+    <Container>
+      <h2>
+        Prazer em te conhecer, sou o <span>Enderson</span>, um apaixonado por
+        códigos, inovações tecnlógicas e muito café.
+        <br />
+        Formado em Análise e Desenvolvimento de Sistemas pelo Insituto Federal
+        de Rondônia, sou focado em desenvolvimento front-end e motivado pela
+        possibilidade que a tecnologia nos proporciona de criar soluções para os
+        mais diversos problemas.
+        <br />
+        Desde criança sempre tive interesse em computadores e tecnologia, e hoje
+        em dia não é diferente, estou sempre em busca de novos conhecimentos e
+        novas tecnologias para me aprimorar e me tornar um profissional melhor a
+        cada dia. Depois que descobri a programação, não consegui mais parar,
+        por mais que seja um caminho árduo, é um caminho que eu amo percorrer.
+        <br />
+        Dia após dia, foco no meu crescimento profissional e pessoal, aprendendo
+        e me aprimorando cada vez mais, e no meu tempo livre gosto de jogar um
+        futebol, viodegame, praticar musculação, fazer alguns tutoriais para o
+        YouTube, ouvir música, tocar violão por mais que eu não seja bom estou
+        evoluindo, e claro, tomar um café.
+        <br />
+        No início, comecei a estudar Python e gostei, mas depois de um tempo
+        percebi que o que eu realmente gostava era do desenvolvimento front-end,
+        fazer animações, criar interfaces, e hoje em dia estou focado em
+        desenvolvimento web, mas logo logo pretendo me tornar um desenvolvedor
+        full-stack.
+      </h2>
+    </Container>
+  );
+}
